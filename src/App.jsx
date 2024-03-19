56b27b47147bb0b947cc97a01a7c87738940edbb
vrundanawal12 <vrunda.nawal@cognizant.com>
2024-03-19 14:38:34 +0000

    pass the props and methods to the InputBox

--- src/App.jsx
+++ src/App.jsx
@@ -32,17 +32,25 @@ function App() {
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
         backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
-    }}
+      }}
     >
       <div className="w-full">
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
           <form
             onSubmit={(e) => {
               e.preventDefault();
+              convert();
             }}
           >
             <div className="w-full mb-1">
-              <InputBox label="From" />
+              <InputBox
+                label="From"
+                amount={amount}
+                onAmountChange={(amount) => setAmount(amount)}
+                onCurrencyChange={(currency) => setAmount(amount)}
+                currencyOptions={options}
+                selectCurrency={from}
+              />
             </div>
             <div className="relative w-full h-0.5">
               <button
