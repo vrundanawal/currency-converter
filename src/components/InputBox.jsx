3d9c6350467d445ea4251c1ad87d0d99541fcfd3
vrundanawal12 <vrunda.nawal@cognizant.com>
2024-03-19 13:39:20 +0000

    Update the InputBox Component with using the dynamic values for the component

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -5,7 +5,7 @@ const InputBox = ({
   amount,
   onAmountChange,
   onCurrencyChange,
-  currencyOption = [],
+  currencyOptions = [],
   selectCurrency = "usd",
   amountDisable = false,
   currencyDisable = false,
@@ -19,12 +19,27 @@ const InputBox = ({
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) =>
+            //convert the string input value into number format with Number method
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
         />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
-        <select className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none">
-          <option value="usd">usd</option>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={selectCurrency}
+          disabled={currencyDisable}
+          onChange={() => onCurrencyChange && onCurrencyChange(e.target.value)}
+        >
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
         </select>
       </div>
     </div>
