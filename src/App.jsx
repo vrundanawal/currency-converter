df02d3c6022d51280a7a0896287debddb511b729
vrundanawal12 <vrunda.nawal@cognizant.com>
2024-03-19 13:54:31 +0000

    create all necessory state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import{InputBox} from './components/index'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState('usd');
+  const [to,setTo] = useState('inr');
+  const[convertedAmount, setConvertedAmount]= useState(0)
+
 
   return (
     <>
